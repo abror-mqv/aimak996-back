0f7edda68095a3fd5ef514984a7fdfbc56f36a86
adymar73583418 <adymarr2005@gmail.com>
2025-06-17 17:10:39 +0600

    213

--- categories/admin.py
+++ categories/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Category, CityBoard, PinnedMessage
+from .models import Category, CityBoard, PinnedMessage, Contacts
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(CityBoard)
 admin.site.register(PinnedMessage)
+admin.site.register(Contacts)
