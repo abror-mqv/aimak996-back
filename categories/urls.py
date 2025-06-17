9a2b2966ff8c1ffa56512349a4dad357e941917e
adymar73583418 <adymarr2005@gmail.com>
2025-06-17 17:30:51 +0600

    erwr

--- categories/urls.py
+++ categories/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CategoryListView, CityBoardListView, PinnedMessageListView, ContactInfoView
 
 urlpatterns = [
-    path('categories/', CategoryListView.as_view(), name='category-list'),
+    path('get/', CategoryListView.as_view(), name='category-list'),
     path('city-boards/', CityBoardListView.as_view(), name='city-board-list'),
     path('pinned-messages/', PinnedMessageListView.as_view(), name='pinned-message-list'),
     path('contact-info/', ContactInfoView.as_view(), name='contact-info'),
