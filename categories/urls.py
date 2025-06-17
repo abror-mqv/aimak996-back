caea48735e3d255975e4cf8719a9087bd1210829
adymar73583418 <adymarr2005@gmail.com>
2025-06-17 19:54:55 +0600

    324

--- categories/urls.py
+++ categories/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import CategoryListView, CityBoardListView, PinnedMessageListView, ContactInfoView
+from .views import CategoryListView, CityBoardListView, ContactInfoView, PinnedMessageByCityView, PinnedMessageListView, CreatePinnedMessageView, DeactivatePinnedMessageView
 
 urlpatterns = [
-    path('get/', CategoryListView.as_view(), name='category-list'),
-    path('city-boards/', CityBoardListView.as_view(), name='city-board-list'),
-    path('pinned-messages/', PinnedMessageListView.as_view(), name='pinned-message-list'),
+    path('get', CategoryListView.as_view()),
+    path("city-boards/", CityBoardListView.as_view(), name="city-board-list"),
+    path("pinned-message/", PinnedMessageByCityView.as_view(), name="pinned-message"),
+    path("pinned-message-list/", PinnedMessageListView.as_view(), name="pinned-message-list"),
+    path("create-pinned-message/", CreatePinnedMessageView.as_view(), name="create-pinned-message"),
+    path("deactivate-pinned-message/<int:pk>/", DeactivatePinnedMessageView.as_view(), name="deactivate-pinned-message"),
     path('contact-info/', ContactInfoView.as_view(), name='contact-info'),
-    # path('pinned-messages/<int:pk>/', PinnedMessageDetailView.as_view(), name='pinned-message-detail'),
 ]
+
